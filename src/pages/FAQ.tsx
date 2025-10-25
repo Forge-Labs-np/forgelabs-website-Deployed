60ae6e63a238777d3949a19a4fc5f689ce0dbc7b
rabinstar137 <rabinstar137@gmail.com>
2025-10-25 15:15:33 +0000

    Scroll to top on FAQ page

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -1,7 +1,11 @@
 import FAQSection from "@/components/FAQSection";
 import { Helmet } from "react-helmet-async";
+import { useEffect } from "react";
 
 const FAQPage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <Helmet>
