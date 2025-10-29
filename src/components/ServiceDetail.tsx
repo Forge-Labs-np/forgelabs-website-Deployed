2a52024bc4a9d39a8fe93ec6153dce38ca26ecfb
rabinstar137 <rabinstar137@gmail.com>
2025-10-29 16:07:44 +0000

    fix: revert image optimization and fix animations

--- src/components/ServiceDetail.tsx
+++ src/components/ServiceDetail.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
-import myImageLoader from '../../loader'; // Assuming loader is in the root
 
 interface ServiceDetailProps {
   title: string;
@@ -13,7 +11,7 @@ const ServiceDetail: React.FC<ServiceDetailProps> = ({ title, description, image
   return (
     <div className={`flex flex-col md:flex-row items-center justify-between py-4 ${reverse ? 'md:flex-row-reverse' : ''}`}>
       <div className="md:w-1/2 p-4" data-aos={`fade-${reverse ? 'left' : 'right'}`} data-aos-once="true">
-        <Image loader={myImageLoader} src={imageUrl} alt={title} width={500} height={300} className="rounded-lg max-w-sm mx-auto mix-blend-multiply" />
+        <img src={imageUrl} alt={title} className="rounded-lg max-w-sm mx-auto mix-blend-multiply" />
       </div>
       <div className="md:w-1/2 p-4" data-aos={`fade-${reverse ? 'right' : 'left'}`} data-aos-once="true">
         <h3 className="text-2xl font-bold mb-4">{title}</h3>
